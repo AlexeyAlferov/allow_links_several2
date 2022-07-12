221691628ca8d6812506c7f7d174f346f261b940
AlexeyAlferov <89243522+AlexeyAlferov@users.noreply.github.com>
2022-07-12 06:48:02 +0500

    Generated by SqlDBM: GIT TEST 2 - v2 by "alexey.alferov+standent@sqldbm.com"

--- SqlDBM/src/Dim_Order.sql
+++ SqlDBM/src/Dim_Order.sql
@@ -5,8 +5,7 @@
 -- ************************************** Dim_Order
 CREATE TABLE Dim_Order
 (
- OrderId     int NOT NULL,
- OrderNumber string NOT NULL,
+ OrderId int NOT NULL,
  CONSTRAINT PK_DIm_Oreder PRIMARY KEY ( OrderId )
 )
 STAGE_FILE_FORMAT = 
